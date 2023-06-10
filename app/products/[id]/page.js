5a63e0b995d29b3a195c5be41e651fdf8e672d92
John Doe <johndoe@example.com>
2023-06-10 13:21:37 +0530

    b

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,23 +1,29 @@
-"use client"
-import React from 'react'
-import useSWR from 'swr'
+"use client";
+import Image from "next/image";
+import React from "react";
+import useSWR from "swr";
 
-const url = 'https://fakestoreapi.com/products'
+const url = "https://fakestoreapi.com/products";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
-const Pro = ({params}) => {
-    const {id}=params
-    const { data, error } = useSWR(`${url}/${id}`, fetcher)
-    console.log(data);
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const Pro = ({ params }) => {
+  const { id } = params;
+  const { data, error } = useSWR(`${url}/${id}`, fetcher);
+  console.log(data);
 
-    if (error) return <h1>Something went wrong!</h1>
-    if (!data) return <h1>Loading...</h1>
+  if (error) return <h1>Something went wrong!</h1>;
+  if (!data) return <h1>Loading...</h1>;
 
   return (
     <div>
-        <h1>{data.title}</h1>
+      <h1>{data.title}</h1>
+      <Image src={`${data.image}`} alt="efff" height={200} width={200} />
+      <h3>Price: ${data.price}</h3>
+      <h2>{data.description}</h2>
+      <p>rating:{data.rating.rate}</p>
+      <p>total sales : {data.rating.count}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Pro
+export default Pro;
