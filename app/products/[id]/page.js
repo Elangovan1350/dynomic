227a7b18b65389c511cc492f3bf17b927629d0cb
John Doe <johndoe@example.com>
2023-06-10 13:49:32 +0530

    1

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -15,13 +15,13 @@ const Pro = ({ params }) => {
   if (!data) return <h1>Loading...</h1>;
 
   return (
-    <div>
-      <h1>{data.title}</h1>
+    <div className="flex justify-center items-center flex-col h-screen max-w-3xl m-auto">
+      <h1 className="text-3xl font-bold">{data.title}</h1>
       <Image src={`${data.image}`} alt="efff" height={200} width={200} />
-      <h3>Price: ${data.price}</h3>
-      <h2>{data.description}</h2>
-      <p>rating:{data.rating.rate}</p>
-      <p>total sales : {data.rating.count}</p>
+      <h3 className="text-xl font-semibold">Price: ${data.price}</h3>
+      <h2 className="text-lg font-medium">{data.description}</h2>
+      <p className="text-base font-medium">rating:{data.rating.rate}</p>
+      <p className="text-base font-medium">total sales : {data.rating.count}</p>
     </div>
   );
 };
